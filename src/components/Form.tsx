13917f6d1b1328450eb27a0767a29e944952ab3d
matheusvicentesn <matheusvicentesj5@hotmail.com>
2023-03-14 22:13:30 -0300

    base css form and layout

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,15 @@
-import { Button, TextField } from "@mui/material";
-import { Formik, useFormik } from "formik";
+import styled from "@emotion/styled";
+import { Button, InputAdornment, TextField } from "@mui/material";
+import { Box, Container, textAlign } from "@mui/system";
+import { useFormik } from "formik";
+
+const FormStyled = styled("form")({});
 
 export const Form = () => {
   const formik = useFormik({
     initialValues: {
       placa: "",
+      modelo: "",
       capacidade: "",
       carga: "",
       consumo: "",
@@ -15,42 +20,102 @@ export const Form = () => {
     },
   });
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
-        <TextField
-          id="placa"
-          name="placa"
-          label="Placa"
-          onChange={formik.handleChange}
-        ></TextField>
-        <TextField
-          id="capacidade"
-          name="capacidade"
-          label="Capacidade do tanque"
-          onChange={formik.handleChange}
-        ></TextField>
-        <TextField
-          id="carga"
-          name="carga"
-          label="Carga máxima"
-          onChange={formik.handleChange}
-        ></TextField>
-        <TextField
-          id="consumo"
-          name="consumo"
-          label="Consumo médio"
-          onChange={formik.handleChange}
-        ></TextField>
-        <TextField
-          id="distancia"
-          name="distancia"
-          label="Distância percorrida na jornada"
-          onChange={formik.handleChange}
-        ></TextField>
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Submit
-        </Button>
-      </form>
-    </div>
+    <Container
+      maxWidth="sm"
+      sx={{ border: 1, borderColor: "#00A335", height: "80vh" }}
+    >
+      <h1>Calcule</h1>
+      <FormStyled>
+        <form onSubmit={formik.handleSubmit}>
+          <TextField
+            id="placa"
+            name="placa"
+            label="Placa"
+            fullWidth
+            margin="dense"
+            onChange={formik.handleChange}
+          ></TextField>
+          <TextField
+            id="modelo"
+            name="modelo"
+            label="Modelo"
+            fullWidth
+            margin="dense"
+            onChange={formik.handleChange}
+          ></TextField>
+          <TextField
+            id="capacidade"
+            name="capacidade"
+            label="Capacidade do tanque"
+            fullWidth
+            margin="dense"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">Litros</InputAdornment>
+              ),
+            }}
+            onChange={formik.handleChange}
+          ></TextField>
+          <TextField
+            id="carga"
+            name="carga"
+            label="Carga máxima"
+            fullWidth
+            margin="dense"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">Toneladas</InputAdornment>
+              ),
+            }}
+            onChange={formik.handleChange}
+          ></TextField>
+          <TextField
+            id="consumo"
+            name="consumo"
+            label="Consumo médio"
+            fullWidth
+            margin="dense"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">Litros</InputAdornment>
+              ),
+            }}
+            onChange={formik.handleChange}
+          ></TextField>
+          <TextField
+            id="distancia"
+            name="distancia"
+            label="Distância percorrida na jornada"
+            margin="dense"
+            onChange={formik.handleChange}
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">Km</InputAdornment>
+              ),
+            }}
+          ></TextField>
+          <Box
+            component="span"
+            m={5}
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Button color="primary" variant="contained" type="submit">
+              Calcular
+            </Button>
+            <Button color="primary" variant="contained">
+              Limpar Campos
+            </Button>
+            <Button color="primary" variant="contained">
+              Histórico
+            </Button>
+          </Box>
+        </form>
+
+        <h2> O resultado é: </h2>
+      </FormStyled>
+    </Container>
   );
 };
